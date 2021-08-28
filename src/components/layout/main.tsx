7a56134bfcf938b7d3f18d477a1e5a50a7e47cda
Petter <petter.fogel@medieinstitutet.se>
2021-08-28 21:16:41 +0200

    Add switch and route to handle components path url.

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -1,12 +1,21 @@
 import "../../styles/main.css";
-import MovieList from "../movies/movieList";
+import MovieCategory from "../movies/movieCategory";
+import { Switch, Route } from "react-router-dom";
+import SearchMovie from "../movies/searchMovie";
 
 function Movies() {
-    return (
-        <main>
-            <MovieList />
-        </main>
-    );
+  return (
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <MovieCategory />
+        </Route>
+        <Route path="/search-movie">
+          <SearchMovie />
+        </Route>
+      </Switch>
+    </main>
+  );
 }
 
-export default Movies
+export default Movies;
