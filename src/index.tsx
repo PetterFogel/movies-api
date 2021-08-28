697935b7a2509216e1353543e656dae2ba3ea715
Petter <petter.fogel@medieinstitutet.se>
2021-08-28 21:15:59 +0200

    Add browserRouter.

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { MovieContextProvider } from './context/movieContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MovieContextProvider>
-      <App />
-    </MovieContextProvider>
+    <BrowserRouter>
+      <MovieContextProvider>
+        <App />
+      </MovieContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
