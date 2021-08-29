014005bbabdee4daf1dc4e93ef6d6e48b441f6b5
Petter <petter.fogel@medieinstitutet.se>
2021-08-29 16:14:34 +0200

    Renamed component and add logic to handle error and loading.

---
+++ src/components/movies/movieCategory.tsx
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import MovieItem from "./movieItem";
+import MovieContext from "../../context/movieContext";
+import "../../styles/movieCategory.css";
+
+function MovieCategory() {
+  const { movieList, isLoading, fetchMovies } = useContext(MovieContext);
+  const [error, setError] = useState(false);
+  const [filter, setFilter] = useState("popular");
+
+  useEffect(() => {
+    fetchMovies(filter);
+
+    if (movieList.length <= 0) {
+      setError(true);
+    }
+    setError(false);
+  }, [filter]);
+
+  function sortingHandler(category: string) {
+    setFilter(category);
+  }
+
+  return (
+    <section>
+      <div className="category-btn-holder">
+        <button
+          className="category-btn"
+          onClick={() => sortingHandler("popular")}
+        >
+          Popular
+        </button>
+        <button
+          className="category-btn"
+          onClick={() => sortingHandler("top_rated")}
+        >
+          Top Rated
+        </button>
+        <button
+          className="category-btn"
+          onClick={() => sortingHandler("upcoming")}
+        >
+          Upcoming
+        </button>
+      </div>
+
+      {isLoading && <p className="loading">Loading...</p>}
+      {!error && (
+        <div className="movie-list-container">
+          {movieList.map((movie: any) => (
+            <MovieItem {...movie} key={movie.id} />
+            ))}
+        </div>
+      )}
+      {error && <p className="loading">Something went wrong...</p>}
+    </section>
+  );
+}
+
+export default MovieCategory;
