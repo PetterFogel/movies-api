dfc0abea1ffd9bf259c80a6c81f9eafd625633a8
Petter <petter.fogel@medieinstitutet.se>
2021-08-29 16:15:35 +0200

    Add link to navigate to movieDetails by id.

--- src/components/movies/movieItem.tsx
+++ src/components/movies/movieItem.tsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
 import "../../styles/movieItem.css";
 import MovieCard from "../ui/movieCard";
 
 function MovieItem(props: any) {
-
     const moviePoster = `https://image.tmdb.org/t/p/w1280/${props.poster_path}`;
 
     return(
         <MovieCard>
-            <img src={moviePoster} alt={props.title} className="image-holder" />
-            <div className="movie-info">
+            <Link to={`/movie-details/${props.id}`} style={{ textDecoration: "none" }}>
+                <img src={moviePoster} alt={props.title} className="image-holder" />
+                <div className="movie-info">
                 <p className="title">{props.title}</p>
                 <p className="rating">{props.vote_average}</p>
-            </div>
+                </div>
+            </Link>
         </MovieCard>
     );
 }
