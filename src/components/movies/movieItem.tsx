3913759eca41324fe2a7d314efcba2b3c5dcd38e
Petter <petter.fogel@medieinstitutet.se>
2021-08-30 20:17:53 +0200

    Add useHistory to navigate to movieDetails component.

--- src/components/movies/movieItem.tsx
+++ src/components/movies/movieItem.tsx
@@ -1,21 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Movie } from "../../models/movie";
 import "../../styles/movieItem.css";
 import MovieCard from "../ui/movieCard";
 
-function MovieItem(props: any) {
-    const moviePoster = `https://image.tmdb.org/t/p/w1280/${props.poster_path}`;
+function MovieItem(props: Movie) {
+  const history = useHistory();
+  const moviePoster = `https://image.tmdb.org/t/p/w1280/${props.poster_path}`;
 
-    return(
-        <MovieCard>
-            <Link to={`/movie-details/${props.id}`} style={{ textDecoration: "none" }}>
-                <img src={moviePoster} alt={props.title} className="image-holder" />
-                <div className="movie-info">
-                <p className="title">{props.title}</p>
-                <p className="rating">{props.vote_average}</p>
-                </div>
-            </Link>
-        </MovieCard>
-    );
+    function handleClick() {
+        history.push(`movie-details/${props.id}`)
+    }
+
+  return (
+    <MovieCard>
+        <img 
+          src={moviePoster} 
+          alt={props.title} 
+          className="image-holder"
+          onClick={handleClick}
+        />
+        <div className="movie-info" onClick={handleClick}>
+          <p className="title">{props.title}</p>
+          <p className="rating">{props.vote_average}</p>
+        </div>
+    </MovieCard>
+  );
 }
 
 export default MovieItem;
