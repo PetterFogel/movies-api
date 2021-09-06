f427c4976b7915decbfe0fcee539824965de1fda
Petter <petter.fogel@medieinstitutet.se>
2021-09-06 19:36:38 +0200

    Removed Link from router dom and add history.push

--- src/components/movies/movieItem.tsx
+++ src/components/movies/movieItem.tsx
@@ -1,4 +1,4 @@
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Movie } from "../../models/movie";
 import "../../styles/movieItem.css";
 import MovieCard from "../ui/movieCard";
