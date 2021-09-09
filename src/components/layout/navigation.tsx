235475fd5cb55bdd003729741916d5d6f2729f88
Petter <petter.fogel@medieinstitutet.se>
2021-09-09 20:40:21 +0200

    Add logic to toggle navbar.

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navigation.css";
 
 function Navigation() {
+  const [toggle, setToggle] = useState(false);
+
+  function navToggleHandler() {
+    setToggle(!toggle);
+  }
+
+  const navCloseHandler = () => {
+    setToggle(false);
+  };
+
   return (
     <header>
-      <h2 className="logo">MovieBox</h2>
+      <h2 className="logo">Moviebox</h2>
       <nav>
-        <ul className="nav-links">
-          <Link to="/" style={{ textDecoration: "none" }}>
+        <ul
+          className="nav-links"
+          style={{
+            right: toggle ? "0%" : "-100%",
+          }}
+        >
+          <Link
+            to="/"
+            style={{ textDecoration: "none" }}
+            onClick={navCloseHandler}
+          >
             <li>Movies</li>
           </Link>
-          <Link to="/favorites" style={{ textDecoration: "none" }}>
+          <Link
+            to="/favorites"
+            style={{ textDecoration: "none" }}
+            onClick={navCloseHandler}
+          >
             <li>Favorites</li>
           </Link>
-          <Link to="/search-movie" style={{ textDecoration: "none" }}>
+          <Link
+            to="/search-movie"
+            style={{ textDecoration: "none" }}
+            onClick={navCloseHandler}
+          >
             <li>Search</li>
           </Link>
         </ul>
       </nav>
-        <div className="burger-menu">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+      <div className="burger-menu" onClick={navToggleHandler}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </header>
   );
 }
