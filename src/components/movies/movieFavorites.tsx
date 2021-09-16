e3ec35c86040c43984c61563213ecfbcff790e40
Petter <petterfogel96@gmail.com>
2021-09-16 19:36:18 +0200

    Add logic to handle if favoritelist is empty.

--- src/components/movies/movieFavorites.tsx
+++ src/components/movies/movieFavorites.tsx
@@ -1,12 +1,21 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import MovieContext from "../../context/movieContext"
 import MovieItem from "./movieItem";
 
 function MovieFavorites() {
+    const [isEmpty, setIsEmpty] = useState(false);
     const { favoritesList } = useContext(MovieContext);
+
+
+    useEffect(() => {
+        if (favoritesList.length <= 0) {
+            setIsEmpty(true);
+        }
+    }, [favoritesList.length]);
 
     return (
         <section>
+            {isEmpty && <p className="empty-msg">Please add a movie to your favorite list.</p>}
             <div className="movie-list-container">
                 {favoritesList.map((movie) => (
                     <MovieItem {...movie} key={movie.id} />
